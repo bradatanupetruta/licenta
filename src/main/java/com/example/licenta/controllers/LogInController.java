d7637f80b632ff72b031cd4d6d6ef8b45cad8df3
bradatanupetruta <bradatanu.petruta@gmail.com>
2022-04-09 16:59:43 +0300

    Login mock security + UI rest communication

--- src/main/java/com/example/licenta/controllers/LogInController.java
+++ src/main/java/com/example/licenta/controllers/LogInController.java
@@ -5,14 +5,16 @@ import com.example.licenta.services.LogInServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/login")
+@RestController
 public class LogInController {
 
     public LogInServices logInServices;
 
     @GetMapping("/cont")
-    public Cont getCont(){
-        return new Cont();
+    public Cont getCont() {
+        Cont cont = new Cont();
+        cont.setPin("1234");
+        return cont;
         //logInServices.
     }
 }
